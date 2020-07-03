8cf2a6effb9fb6bab84ea162cb61cba45231becf
Alekos Filini <alekos.filini@gmail.com>
2020-07-03 12:31:58 +0200

    Initial commit

---
+++ android/app/src/main/java/com/react_native_demo/DemoPackage.java
@@ -0,0 +1,29 @@
+package com.react_native_demo;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DemoPackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        //this is where you register the module
+        modules.add(new ReactModule(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}

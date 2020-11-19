50ae7bd144ef2228218d36e10845295c108f6eed
Zoe Faltibà <zoefaltiba@gmail.com>
2020-11-19 18:13:49 +0100

    demo nodejs: use middleware interface

--- demo/nodejs/example.js
+++ demo/nodejs/example.js
@@ -1,4 +1,5 @@
-const ex = require('../../ffi/nodejs/build/Release/rgb_node')
+const fs = require('fs')
+const rgbNode = require('../../ffi/nodejs/rgb_node')
 
 const config = {
     network: "testnet",
@@ -20,6 +21,8 @@ const issueData = {
     prune_seals: [],
 }
 
+const consignmentPath = '/tmp/rgb-node/output/consignment'
+
 const transferData = {
     inputs: ["0313ba7cfcaa66029a1a63918ebc426259f00953016c461663315d1bf6b83ab4:0"],
     allocate: [
@@ -27,34 +30,36 @@ const transferData = {
     ],
     invoice: "rgb20:outpoint1mzu8vz3jly3rzzkdpph583yahv9wktljtfcln6pe2le6n7ehqulstu967t?amount=5&asset=rgb:id1yqqqxya60n725eszngdx8yvwh3pxyk0sp9fszmzxze3nzhgm76ur4dqf2f7gy",
     prototype_psbt: "cHNidP8BAFICAAAAAZ38ZijCbFiZ/hvT3DOGZb/VXXraEPYiCXPfLTht7BJ2AQAAAAD/////AfA9zR0AAAAAFgAUezoAv9wU0neVwrdJAdCdpu8TNXkAAAAATwEENYfPAto/0AiAAAAAlwSLGtBEWx7IJ1UXcnyHtOTrwYogP/oPlMAVZr046QADUbdDiH7h1A3DKmBDck8tZFmztaTXPa7I+64EcvO8Q+IM2QxqT64AAIAAAACATwEENYfPAto/0AiAAAABuQRSQnE5zXjCz/JES+NTzVhgXj5RMoXlKLQH+uP2FzUD0wpel8itvFV9rCrZp+OcFyLrrGnmaLbyZnzB1nHIPKsM2QxqT64AAIABAACAAAEBKwBlzR0AAAAAIgAgLFSGEmxJeAeagU4TcV1l82RZ5NbMre0mbQUIZFuvpjIBBUdSIQKdoSzbWyNWkrkVNq/v5ckcOrlHPY5DtTODarRWKZyIcSEDNys0I07Xz5wf6l0F1EFVeSe+lUKxYusC4ass6AIkwAtSriIGAp2hLNtbI1aSuRU2r+/lyRw6uUc9jkO1M4NqtFYpnIhxENkMak+uAACAAAAAgAAAAAAiBgM3KzQjTtfPnB/qXQXUQVV5J76VQrFi6wLhqyzoAiTACxDZDGpPrgAAgAEAAIAAAAAAACICA57/H1R6HV+S36K6evaslxpL0DukpzSwMVaiVritOh75EO3kXMUAAACAAAAAgAEAAIAA",
-    consignment_file: "/tmp/rgb-node/output/consignment",
+    consignment_file: consignmentPath,
     transaction_file: "/tmp/rgb-node/output/transaction"
 }
 
 var runtime = null
 
 async function main() {
-    await ex.start_rgb(
-        config.network, config.stash_endpoint, JSON.stringify(config.contract_endpoints), config.threaded, config.datadir)
+    await rgbNode.startRgb(
+        config.network, config.stash_endpoint, config.contract_endpoints, config.threaded, config.datadir)
     .then(r => {
         runtime = r
-        return ex.issue(runtime, JSON.stringify(issueData))
+        return rgbNode.issue(runtime, issueData)
     })
     /*
     .then(() => {
-        return ex.transfer(runtime, JSON.stringify(transferData.inputs), JSON.stringify(transferData.allocate),
+        return rgbNode.transfer(runtime, transferData.inputs, transferData.allocate,
            transferData.invoice, transferData.prototype_psbt, transferData.consignment_file,
            transferData.transaction_file)
     })
     .then(() => {
-        return ex.asset_allocations(runtime, 'rgb1w82xuaxz6lp9symrp3f4r47rylkkxsh506qzkt2n2kjfhrhrt03qrrcm0g')
+        return rgbNode.assetAllocations(runtime, 'rgb1w82xuaxz6lp9symrp3f4r47rylkkxsh506qzkt2n2kjfhrhrt03qrrcm0g')
     })
     */
     .then(() => {
-        return ex.outpoint_assets(runtime, '5aa2d0a8098371ee12b4b59f43ffe6a2de637341258af65936a5baa01da49e9b:0')
+        return rgbNode.outpointAssets(runtime, '5aa2d0a8098371ee12b4b59f43ffe6a2de637341258af65936a5baa01da49e9b:0')
     })
     .then(assets => {
         console.log('assets: ' + assets)
+        consignment = fs.readFileSync(consignmentPath)
+        return rgbNode.validate(runtime, consignment)
     })
 }
 

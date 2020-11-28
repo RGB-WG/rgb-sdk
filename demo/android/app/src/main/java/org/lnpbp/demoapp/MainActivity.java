3824b4e66e57e86ae67316130e6f0ac5ccd627a7
Zoe Faltibà <zoefaltiba@gmail.com>
2020-11-28 17:10:27 +0100

    demo android: fix and improve

--- demo/android/app/src/main/java/org/lnpbp/demoapp/MainActivity.java
+++ demo/android/app/src/main/java/org/lnpbp/demoapp/MainActivity.java
@@ -1,16 +1,16 @@
 package org.lnpbp.demoapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.lnpbp.rgbnode.Runtime;
 import org.lnpbp.rgbnode.model.IssueArgs;
@@ -21,6 +21,8 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,27 +30,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        final FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Runtime runtime = ((DemoApp) getApplication()).getRuntime();
-                try {
-                    final IssueArgs.CoinAllocation allocation = new IssueArgs.CoinAllocation(100L, 0, "0313ba7cfcaa66029a1a63918ebc426259f00953016c461663315d1bf6b83ab4");
-                    runtime.issue("bitcoin", "USDT", "USD Tether", null, "SingleIssue", Arrays.asList(allocation), 8, new ArrayList(), null);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final DemoApp app = (DemoApp) getApplication();
+                        final Runtime runtime = app.getRuntime();
+                        try {
+                            final IssueArgs.CoinAllocation allocation = new IssueArgs.CoinAllocation(
+                                100L,
+                                0,
+                                "0313ba7cfcaa66029a1a63918ebc426259f00953016c461663315d1bf6b83ab4");
+                            runtime.issue(
+                                app.network,"USDT","USD Tether",null,
+                                "SingleIssue", Arrays.asList(allocation),8,
+                                new ArrayList()
+                            );
+                            Log.d(TAG, "issue completed");
 
-                    final String consignment_file = getCacheDir().toString() + "/" + UUID.randomUUID().toString();
-                    final String transaction_file = getCacheDir().toString() + "/" + UUID.randomUUID().toString();
-                    runtime.transfer(
-                            Arrays.asList("0313ba7cfcaa66029a1a63918ebc426259f00953016c461663315d1bf6b83ab4:0"),
-                            Arrays.asList(allocation),
-                            "rgb20:outpoint1mzu8vz3jly3rzzkdpph583yahv9wktljtfcln6pe2le6n7ehqulstu967t?amount=5&asset=rgb:id1yqqqxya60n725eszngdx8yvwh3pxyk0sp9fszmzxze3nzhgm76ur4dqf2f7gy",
-                            "cHNidP8BAFICAAAAAZ38ZijCbFiZ/hvT3DOGZb/VXXraEPYiCXPfLTht7BJ2AQAAAAD/////AfA9zR0AAAAAFgAUezoAv9wU0neVwrdJAdCdpu8TNXkAAAAATwEENYfPAto/0AiAAAAAlwSLGtBEWx7IJ1UXcnyHtOTrwYogP/oPlMAVZr046QADUbdDiH7h1A3DKmBDck8tZFmztaTXPa7I+64EcvO8Q+IM2QxqT64AAIAAAACATwEENYfPAto/0AiAAAABuQRSQnE5zXjCz/JES+NTzVhgXj5RMoXlKLQH+uP2FzUD0wpel8itvFV9rCrZp+OcFyLrrGnmaLbyZnzB1nHIPKsM2QxqT64AAIABAACAAAEBKwBlzR0AAAAAIgAgLFSGEmxJeAeagU4TcV1l82RZ5NbMre0mbQUIZFuvpjIBBUdSIQKdoSzbWyNWkrkVNq/v5ckcOrlHPY5DtTODarRWKZyIcSEDNys0I07Xz5wf6l0F1EFVeSe+lUKxYusC4ass6AIkwAtSriIGAp2hLNtbI1aSuRU2r+/lyRw6uUc9jkO1M4NqtFYpnIhxENkMak+uAACAAAAAgAAAAAAiBgM3KzQjTtfPnB/qXQXUQVV5J76VQrFi6wLhqyzoAiTACxDZDGpPrgAAgAEAAIAAAAAAACICA57/H1R6HV+S36K6evaslxpL0DukpzSwMVaiVritOh75EO3kXMUAAACAAAAAgAEAAIAA",
-                            consignment_file,
-                            transaction_file);
-                } catch (RuntimeException e) {
-                    Log.e("RGB_NODE", e.getMessage());
-                }
+                            final String consignment_file = app.dataDir + "/" + UUID.randomUUID().toString();
+                            final String transaction_file = app.dataDir + "/" + UUID.randomUUID().toString();
+                            final String invoice = "rgb20:outpoint1mzu8vz3jly3rzzkdpph583yahv9wktljtfcln6pe2le6n7ehqulstu967t?amount=5&asset=rgb:id1yqqqxya60n725eszngdx8yvwh3pxyk0sp9fszmzxze3nzhgm76ur4dqf2f7gy";
+                            final String prototypePsbt = "cHNidP8BAFICAAAAAZ38ZijCbFiZ/hvT3DOGZb/VXXraEPYiCXPfLTht7BJ2AQAAAAD/////AfA9zR0AAAAAFgAUezoAv9wU0neVwrdJAdCdpu8TNXkAAAAATwEENYfPAto/0AiAAAAAlwSLGtBEWx7IJ1UXcnyHtOTrwYogP/oPlMAVZr046QADUbdDiH7h1A3DKmBDck8tZFmztaTXPa7I+64EcvO8Q+IM2QxqT64AAIAAAACATwEENYfPAto/0AiAAAABuQRSQnE5zXjCz/JES+NTzVhgXj5RMoXlKLQH+uP2FzUD0wpel8itvFV9rCrZp+OcFyLrrGnmaLbyZnzB1nHIPKsM2QxqT64AAIABAACAAAEBKwBlzR0AAAAAIgAgLFSGEmxJeAeagU4TcV1l82RZ5NbMre0mbQUIZFuvpjIBBUdSIQKdoSzbWyNWkrkVNq/v5ckcOrlHPY5DtTODarRWKZyIcSEDNys0I07Xz5wf6l0F1EFVeSe+lUKxYusC4ass6AIkwAtSriIGAp2hLNtbI1aSuRU2r+/lyRw6uUc9jkO1M4NqtFYpnIhxENkMak+uAACAAAAAgAAAAAAiBgM3KzQjTtfPnB/qXQXUQVV5J76VQrFi6wLhqyzoAiTACxDZDGpPrgAAgAEAAIAAAAAAACICA57/H1R6HV+S36K6evaslxpL0DukpzSwMVaiVritOh75EO3kXMUAAACAAAAAgAEAAIAA";
+                            final String[] inputs = new String[]{"0313ba7cfcaa66029a1a63918ebc426259f00953016c461663315d1bf6b83ab4:0"};
+                            runtime.transfer(
+                                Arrays.asList(inputs), Arrays.asList(allocation), invoice,
+                                prototypePsbt, consignment_file, transaction_file);
+                            Log.d(TAG, "transfer completed");
+                        } catch (Exception e) {
+                            Log.e(TAG, e.getMessage());
+                        }
+                    }
+                }).start();
+
             }
         });
     }

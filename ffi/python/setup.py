7f67eabc3e40899a4e49fb17c9d87b0cbabae23e
Zoe Faltibà <zoefaltiba@gmail.com>
2020-12-10 11:15:12 +0100

    ffi python: improve build failure on setup

--- ffi/python/setup.py
+++ ffi/python/setup.py
@@ -4,6 +4,8 @@
 Bundle RGB Python bindings with setuptools
 """
 
+import sys
+
 from setuptools.command.build_ext import build_ext
 # keep Extension import after setuptools to avoid class replacement
 # see https://github.com/pypa/setuptools/issues/309
@@ -11,6 +13,12 @@ from distutils.core import Extension, setup
 from subprocess import Popen, TimeoutExpired
 
 RUST_LIB = '../../rust-lib'
+
+
+def _die(message):
+    """ Prints message to stderr with error code 1 """
+    sys.stderr.write(message + '\n')
+    sys.exit(1)
 
 
 def build_rust():
@@ -21,6 +29,9 @@ def build_rust():
         _, _ = proc.communicate(timeout=600)
     except TimeoutExpired:
         proc.kill()
+        _die('Build of Rust library timed out')
+    if proc.returncode:
+        _die('Build of Rust library failed')
 
 
 class BuildExt(build_ext):
@@ -34,6 +45,7 @@ class BuildExt(build_ext):
     def run(self):
         """ Build Rust library and Python extensions """
         build_rust()
+        self.force = True
         build_ext.run(self)
 
 

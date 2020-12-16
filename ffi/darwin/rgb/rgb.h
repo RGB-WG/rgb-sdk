4d44c185970774846dae609360a1163a777e337b
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-12-16 22:19:16 +0100

    Darwin bindings for librgb

---
+++ ffi/darwin/rgb/rgb.h
@@ -0,0 +1,18 @@
+//
+//  rgb.h
+//  rgb
+//
+//  Created by Maxim Orlovsky on 12/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for rgb.
+FOUNDATION_EXPORT double rgbVersionNumber;
+
+//! Project version string for rgb.
+FOUNDATION_EXPORT const unsigned char rgbVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rgb/PublicHeader.h>
+
+
